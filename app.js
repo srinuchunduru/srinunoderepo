198491ceb10cfe0450c4dde8c66449b81464ccf6
CORP\srichund <srinivas.a.chunduru@capgemini.com>
2019-02-05 17:10:17 +0530

    message change1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000,
     app = express();
 
     app.get('/greet', function(req, res){
-        res.send('Hello world, Greetings!!! Welcome to Node Project')
+        res.send('Hello world, Greetings!!! Welcome ')
       })
       
       app.listen(port, function(){
