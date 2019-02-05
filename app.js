ff91dcff8bba44b0cb38d5a4a602a1ccbe90032f
CORP\srichund <srinivas.a.chunduru@capgemini.com>
2019-02-05 15:07:15 +0530

    node project

---
+++ app.js
@@ -0,0 +1,14 @@
+var port = process.env.PORT || 3000,
+    express = require('express'),
+    app = express();
+
+    app.get('/greet', function(req, res){
+        res.send('Hello world, Greetings!!!')
+      })
+      
+      app.listen(port, function(){
+        console.log('Server listening on ', port);
+      })
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
